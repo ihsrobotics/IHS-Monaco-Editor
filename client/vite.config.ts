bc4d73a682c6ea272ab47f4b8986dfd1648e3207
snow4060 <luohaoyun4068@students.esuhsd.org>
2024-02-03 21:27:10 -0800

    chore: add build sourcemap option

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sourcemaps from "rollup-plugin-sourcemaps";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      onLog(level, log, handler) {
+        if (
+          log.cause &&
+          log.message.includes(`Can't resolve original location of error.`)
+        ) {
+          return;
+        }
+        handler(level, log);
+      },
+      //@ts-expect-error Plugin auto casts to InputPluginOption
+      plugins: [sourcemaps()],
+    },
+  },
+});
